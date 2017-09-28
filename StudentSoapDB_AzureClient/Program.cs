8afe9a88f3b7a9cd5073e037fb3081e8a7d50fbe
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2017-09-28 17:15:14 +0200

    oprette connected reference med wsdl fil navnet via azure web app deployment. Kalder 3 ud af 4 metoder.

--- StudentSoapDB_AzureClient/Program.cs
+++ StudentSoapDB_AzureClient/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StudentSoapDB_AzureClient.ServiceReference1;
 
 namespace StudentSoapDB_AzureClient
 {
@@ -10,6 +11,28 @@ namespace StudentSoapDB_AzureClient
     {
         static void Main(string[] args)
         {
+            using (StudentClient client = new StudentClient("BasicHttpBinding_IStudent"))
+            {
+                //Firkantede klammer efter Student klasse er fordi GetallStudent har Ilist<Student> som datatype. 
+                Student[] allstudents = client.GetAllStudents();
+                foreach (var st in allstudents)
+                {
+                    Console.WriteLine(st.StudentName);
+                }
+
+
+                Console.WriteLine("Studerendes navn med id 4:");
+                Student stu = client.GetStudentById(4);
+                Console.WriteLine(stu.StudentName);
+
+                Console.WriteLine("Studerendes id med navn test");
+                Student stu2 = client.GetStudentByName("test");
+                Console.WriteLine(stu2.StudentId);
+
+
+            }
+
+            Console.ReadLine();
         }
     }
 }
